cde4ab4c6852595ab9b7e874b74fd2a67fc057fe
ertugrullberber <ertugrulberber3461@gmail.com>
2022-06-27 15:06:38 +0300

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Ertugrul Berber on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
